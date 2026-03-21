ada5f25a83ecc0b2c4bd5f7b2625b08b3e001996
pokerfacer9527 <71075428+pokerfacer9527@users.noreply.github.com>
2026-03-21 18:27:51 +0800

    Update init-settings.sh

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -1,5 +1,40 @@
 #!/bin/bash
 
+# 1. 获取所有物理网卡接口（排除 lo 和 虚拟网卡）
+INTERFACES=$(ls /sys/class/net | grep -E 'eth|enp|eno' | sort)
+# 获取网卡总数
+COUNT=$(echo "$INTERFACES" | wc -l)
+
+if [ "$COUNT" -gt 1 ]; then
+    # 最后一个网卡作为 WAN
+    WAN_IF=$(echo "$INTERFACES" | tail -n 1)
+    # 其余网卡作为 LAN
+    LAN_IFS=$(echo "$INTERFACES" | head -n $((COUNT - 1)))
+
+    # 2. 配置 LAN (包含 IP 修改)
+    uci set network.lan.device='br-lan'
+    uci set network.lan.ipaddr='192.168.2.1'
+    uci set network.lan.netmask='255.255.255.0'
+    
+    # 清空旧的 bridge 成员并添加新的
+    uci del network.lan.ports
+    for iface in $LAN_IFS; do
+        uci add_list network.lan.ports="$iface"
+    done
+
+    # 3. 配置 WAN
+    uci set network.wan=interface
+    uci set network.wan.proto='dhcp'
+    uci set network.wan.device="$WAN_IF"
+    
+    # 4. 配置 WAN6 (可选)
+    uci set network.wan6=interface
+    uci set network.wan6.proto='dhcpv6'
+    uci set network.wan6.device="$WAN_IF"
+
+    uci commit network
+fi
+
 # Set default theme to luci-theme-argon
 uci set luci.main.mediaurlbase='/luci-static/argon'
 uci commit luci
