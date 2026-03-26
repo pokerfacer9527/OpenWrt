188d823a0de648b3a1d12e3a651aa060b47d59a9
pokerfacer9527 <71075428+pokerfacer9527@users.noreply.github.com>
2026-03-26 10:41:58 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -72,7 +72,7 @@ git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclas
 #git_sparse_clone main https://github.com/haiibo/packages luci-theme-atmaterial luci-theme-opentomcat luci-theme-netgear
 
 # 更改 Argon 主题背景
-cp -f $GITHUB_WORKSPACE/images/bg1.jpg package/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
+#cp -f $GITHUB_WORKSPACE/images/bg1.jpg package/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
 
 # 晶晨宝盒
 #git_sparse_clone main https://github.com/ophub/luci-app-amlogic luci-app-amlogic
